1ad6ecebe1c48c2881007ae8990cfc3a5c9c76be
Jing George <jing.c.geo@gmail.com>
2020-07-26 16:15:48 -0400

    set up migrations; create student table

---
+++ migrations/SQL/0001_create-student.sql
@@ -0,0 +1,3 @@
+CREATE TABLE IF NOT EXISTS student (
+    id SERIAL PRIMARY KEY
+)
---
+++ migrations/migrate.js
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv';
+import pkg from "postgres-migrations";
+import pg from "pg";
+const  {createDb, migrate} = pkg;
+
+async function runMigrations () {
+    const dbConfig = {
+        database: "acmw",
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        }
+        
+        {
+        const client = new pg.Client({
+            ...dbConfig,
+            database: "postgres",
+        })
+        await client.connect()
+        try {
+            await createDb("acmw", {client})
+        } finally {
+            await client.end()
+        }
+        }
+        
+        {
+        const client = new pg.Client(dbConfig) // or a Pool, or a PoolClient
+        await client.connect()
+        try {
+            await migrate({client}, "./migrations/SQL")
+        } finally {
+            await client.end()
+        }
+        }
+}
+
+runMigrations();
