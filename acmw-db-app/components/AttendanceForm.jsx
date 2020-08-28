bc1c2a8cb1871c6621751e51e250654ed053f898
Sara Miskus <51302701+smiskus@users.noreply.github.com>
2020-08-28 15:49:09 -0400

    Szm/ada 24 attendance form front end (#10)
    * ADA-24 Adding attendance form front UI
    
    * Adding comments, mock functions, and cleaning up code
    
    * indent
    
    * Adding some other validation
    
    Co-authored-by: SnobbyDragon <angela.li.2019@gmail.com>

---
+++ acmw-db-app/components/AttendanceForm.jsx
@@ -0,0 +1,63 @@
+import SelectInput from '../components/FormComponents/SelectInput'
+import TextField from '../components/FormComponents/TextField'
+import SubmitButton from '../components/FormComponents/SubmitButton'
+import styles from '../styles/components/AttendanceForm.module.css'
+import {useState} from 'react'
+import Head from 'next/head'
+
+const AttendanceForm = (props) => {
+
+    const [eventCode, setEventCode] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+
+    const yearOptions = ["First", "Second", "Third", "Fourth", "Fifth+", "Graduate or Phd student :)"]
+    const listServOptions = ["I am already on the list :)", "Yes, please!", "Nope, thank you"]
+
+    const handleSubmit = () => {
+        // If event code is valid, add a database entry, and display "Submitted successfully" message
+        // Else if the event code is not valid, display "Invalid event code" message
+
+        // This would be changed to pull from a database of event codes and see if there is a match (maybe?)
+        if (firstName !== "" && lastName !== "" && eventCode === "007") {
+            console.log("Successful recorded your attendance!")
+        } else if (eventCode === "007") {
+            console.log("You must provide your first and last name!")
+        } else {
+            console.log("Event code is incorrect!")
+        }
+    }
+
+    // Sara: Actually this form might be slightly different depending on how the website is set up.
+    // If ACM-W users are able to login to their personal accounts, then we would just need the event code.
+    // This is just going off of past attendance forms.
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>ACM-W Attendance Form</title>
+            </Head>
+            <div className={styles.card}>
+                <h1>Attendance Form</h1>
+                <p>Thank you for attending an ACM-W event! Please fill out this form to record your attendance!</p>
+                <h2>Event Information</h2>
+                <TextField label="Event Code" onChange={(event) => setEventCode(event.target.value)}/>
+                <TextField label="First Name" onChange={(event) => setFirstName(event.target.value)}/>
+                <TextField label="Last Name.#" onChange={(event) => setLastName(event.target.value)}/>
+                <div>
+                    <h2>What year are you in?</h2>
+                    <SelectInput options={yearOptions}/>
+                </div>
+                <div>
+                    <h2>Would you like to be included in our list-serv?</h2>
+                    <SelectInput options={listServOptions}/>
+                </div>
+                <span>
+                    <SubmitButton label="Submit" handleChange={handleSubmit}/>
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default AttendanceForm
---
+++ acmw-db-app/pages/attendance.js
@@ -0,0 +1,11 @@
+import AttendanceForm from '../components/AttendanceForm'
+
+const Attendance = () => {
+    return (
+        <div>
+            <AttendanceForm />
+        </div>
+    )
+}
+
+export default Attendance
