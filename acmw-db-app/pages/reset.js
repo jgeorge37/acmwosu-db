6d1cfaf73d3014458bb9276f226fd4e70b533dfa
Morgan Zahner <morgan_zahner@yahoo.com>
2020-08-28 11:16:48 -0400

    ADA-21 - remove extra code

--- acmw-db-app/pages/reset.js
+++ acmw-db-app/pages/reset.js
@@ -30,8 +30,6 @@ class ResetPW extends React.Component {
         console.log(this.state);
   
         let input = {};
-        input["name"] = "";
-        input["email"] = "";
         input["password"] = "";
         input["confirm_password"] = "";
         this.setState({input:input});
@@ -44,25 +42,6 @@ class ResetPW extends React.Component {
       let input = this.state.input;
       let errors = {};
       let isValid = true;
-  
-      // if (!input["name"]) {
-      //   isValid = false;
-      //   errors["name"] = "Please enter your name.";
-      // }
-  
-      // if (!input["email"]) {
-      //   isValid = false;
-      //   errors["email"] = "Please enter your email Address.";
-      // }
-  
-      // if (typeof input["email"] !== "undefined") {
-          
-      //   var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-      //   if (!pattern.test(input["email"])) {
-      //     isValid = false;
-      //     errors["email"] = "Please enter valid email address.";
-      //   }
-      // }
   
       <h6>Please enter a password containing at least 1 uppercase, 1 lowercase, 1 digit, 1 special character with a length of at least of 8</h6>
       var pattern = new RegExp(/^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/i);
@@ -105,34 +84,6 @@ class ResetPW extends React.Component {
       <main className={styles.card}>
         <h1>Password Reset</h1>
         <form onSubmit={this.handleSubmit}>
-  
-        {/* { <div className={styles.passwordChunk}>
-            <label className={styles.labelStyle} htmlFor="name">Name:</label>
-            <input 
-              type="text" 
-              name="name" 
-              value={this.state.input.name}
-              onChange={this.handleChange}
-              //class="form-control" 
-              placeholder="Enter name" 
-              id="name" />
-  
-              <div className="text-danger">{this.state.errors.name}</div>
-          </div>
-  
-          <div className={styles.passwordChunk}>
-            <label className={styles.labelStyle} htmlFor="email">Email Address:</label>
-            <input 
-              type="text" 
-              name="email" 
-              value={this.state.input.email}
-              onChange={this.handleChange}
-              //class="form-control" 
-              placeholder="Enter email" 
-              id="email" />
-  
-              <div className="text-danger">{this.state.errors.email}</div>
-          </div> } */}
    
           <div className={styles.passwordRequirements}>
             <ul>Enter a password with at least...
