c7231c889e060e7fa9cacef735706c2e62f172f1
Morgan Zahner <morgan_zahner@yahoo.com>
2020-09-06 13:30:41 -0400

    ADA 5 - check to see if email is empty

--- acmw-db-app/pages/forgotPassword.js
+++ acmw-db-app/pages/forgotPassword.js
@@ -38,7 +38,7 @@ class forgotPassword extends React.Component {
       let errors = {};
       let isValid = true;
 
-      if (typeof input["email"] !== "undefined") {
+      if (typeof input["email"] !== "undefined" || input["email"].length() == 0) {
 
         var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
         if (!pattern.test(input["email"])) {
