9733cdd44ce785a91bb0274368b5505f8fd013c3
Morgan Zahner <morgan_zahner@yahoo.com>
2020-09-09 13:35:59 -0400

    ADA 5 - typeof

--- acmw-db-app/pages/forgotPassword.js
+++ acmw-db-app/pages/forgotPassword.js
@@ -38,7 +38,7 @@ class forgotPassword extends React.Component {
       let errors = {};
       let isValid = false;
 
-        if (input.email !== "undefined" && input.email !== null && input.email !== "") {
+        if (typeof input.email !== "undefined" && input.email !== null && input.email !== "") {
 
         var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
           if (pattern.test(input["email"])) {
