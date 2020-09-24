8e9748bf19c6351429d3a72fa7a41e0f8a3d1f21
Sara Miskus <51302701+smiskus@users.noreply.github.com>
2020-09-23 20:54:05 -0400

    Adding sponsorship options form

---
+++ acmw-db-app/components/SponsorshipForm.jsx
@@ -0,0 +1,44 @@
+import SubmitButton from './FormComponents/SubmitButton'
+import TextField from './FormComponents/TextField'
+import SelectInput from './FormComponents/SelectInput'
+import MultiSelectInput from './FormComponents/MultiSelectInput'
+import styles from '../styles/components/CompanyForm.module.css'
+
+const SponsorshipForm = (props) => {
+
+    const handleChange = () => {
+        console.log("Hello!")
+    }
+
+    const tempOptions = ["Most recent sponsorship date", "Least recent sponsorship date"]
+    const temp2Options = ["All sponsors", "2019-2020 Sponsors", "2018-2019 Sponsors"]
+
+    return (
+        <div className={styles.popup}>
+            <div className={styles.popup_inner}>
+                <form className={styles.form}>
+                    <div>
+                        <h2>Sort by: </h2>
+                        <SelectInput options={tempOptions} />
+                    </div>
+                    <div>
+                        <h2>Show: </h2>
+                        <MultiSelectInput options={temp2Options} />
+                    </div>
+                    <div>
+                        <h2>Search for Company Sponsor: </h2>
+                        <div className={styles.one}>
+                            <TextField label="Company Name"/>
+                        </div>
+                    </div>
+                    <div>
+                        <SubmitButton label="Apply" handleChange={handleChange} />
+                        <SubmitButton label="Cancel" handleChange={props.closeForm} />
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default SponsorshipForm
