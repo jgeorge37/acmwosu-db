8a048d7d5b71ad1486578c8c02d13ab9a1ad8f35
Ang <angela.li.2019@gmail.com>
2020-10-02 18:23:58 -0400

    checks if attended meeting this academic year

--- acmw-db-app/pages/api/student/[pid].js
+++ acmw-db-app/pages/api/student/[pid].js
@@ -2,7 +2,26 @@ import pgQuery from '../../../postgres/pg-query.js';
 
 // GET /api/student/totalUniqueMembers
 async function totalUniqueMembers () {
-    const data = await pgQuery(`SELECT COUNT(DISTINCT name_dot_num) FROM student`);
+    const d = new Date();
+    const year = d.getFullYear() - 2000;
+    const month = d.getMonth();
+    var fall = "AU", spring = "SP";
+    if (8 <= month <= 12) {
+      // it's currrently fall semester
+      fall += year;
+      spring += (year + 1);
+    } else {
+      //it's currrently spring (or summer)
+      fall += (year - 1);
+      spring += year;
+    }
+    const data = await pgQuery(`SELECT COUNT(DISTINCT name_dot_num)
+                                FROM student
+                                INNER JOIN
+                                  meeting_student INNER JOIN meeting
+                                  ON meeting_student.meeting_id=meeting.id
+                                  AND (meeting.semester='${fall}' OR meeting.semester='${spring}')
+                                ON student.id=meeting_student.student_id`);
     return data.rows[0]["count"];
 }
 
