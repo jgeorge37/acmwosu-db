07b58d0d8dc0a407160611e167c19ef962fd15c0
Jing George <jing.c.geo@gmail.com>
2020-10-03 00:54:53 -0400

    ADA-19 Add API endpoint to generate and save pw reset token with expiration time

--- acmw-db-app/pages/api/account/[pid].js
+++ acmw-db-app/pages/api/account/[pid].js
@@ -7,6 +7,28 @@ async function verify (email, password) {
     return data.rows;
 }
 
+// POST /api/account/generate-token
+// Generate a password reset token and set the expiration timestamp
+async function generateToken (email) {
+    let token = Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2);
+
+    // re-generate token if already in table - highly unlikely to happen even once, so runtime in reality is not bad.
+    while((await pgQuery(`SELECT * FROM account WHERE reset_token = '${token}';`)).rows.length > 0) {
+        token = Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2);
+    }
+
+    // save token and set expiration time 2 days in the future
+    const data = await pgQuery(`
+        UPDATE account 
+        SET reset_token = '${token}', token_expire_time = current_timestamp + INTERVAL '2 days'
+        WHERE email = '${email}'
+    `);
+
+    // throw error if the email address doesn't match an account
+    if(data.rowCount === 0) throw (`Account with email ${email} does not exist.`);
+    return data;
+}
+
 export default async (req, res) => {
     const { 
       query: { pid },
@@ -20,6 +42,9 @@ export default async (req, res) => {
     
             if(pid === 'verify') {
                 result = await verify(body.email, body.password);
+            } else if(pid == 'generate-token') {
+                if(!body.email) throw("Missing email address in request body.");
+                result = await generateToken(body.email);
             } else {
                 throw("Invalid pid");
             }
