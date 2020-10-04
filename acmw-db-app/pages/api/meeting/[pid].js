fb660e5fde44336be7d5f451703263c6bf76c5c8
Jing George <jing.c.geo@gmail.com>
2020-10-03 23:58:59 -0400

    ADA-73 Add endpoints for getting attendance for current AUxx and SPxx by account email or student id

--- acmw-db-app/pages/api/meeting/[pid].js
+++ acmw-db-app/pages/api/meeting/[pid].js
@@ -16,6 +16,25 @@ async function averageAttendance () {
     return data.rows[0]["avg"] ? data.rows[0]["avg"] : 0;
 }
 
+// GET /api/meeting/student-attendance
+async function studentAttendance(id) {
+	const [fall, spring] = currentAcademicYear();
+	const numMeetings = await pgQuery(`
+		SELECT m.semester, COUNT(m.id) 
+		FROM meeting m INNER JOIN meeting_student ms ON m.id = ms.meeting_id
+		WHERE ms.student_id = '${id}' AND (m.semester='${fall}' OR m.semester='${spring}')
+		GROUP BY m.semester;
+	`);
+	return numMeetings.rows;
+}
+
+// GET /api/meeting/account-attendance
+async function accountAttendance(email) {
+	const data = await pgQuery(`SELECT s.id FROM student s INNER JOIN account a on s.id = a.student_id WHERE a.email = '${email}'`);
+	if(data.rowCount === 0) throw (`No student associated with account ${email}`);
+	return (await studentAttendance(data.rows[0].id));
+}
+
 export default async (req, res) => {
     const {
       query: { pid },
@@ -27,6 +46,16 @@ export default async (req, res) => {
         if (req.method === 'GET'){
             if (pid === 'averageAttendance') {
                 result = await averageAttendance();
+            } else if (pid === 'account-attendance') {
+				if(!req.query || (!req.query.email)) {
+                    throw("Missing account email in query.");
+                }
+                result = await accountAttendance(req.query.email);
+            } else if (pid === 'student-attendance') {
+				if(!req.query || (!req.query.id)) {
+                    throw("Missing student id in query.");
+                }
+                result = await studentAttendance(req.query.id);
             } else {
                 throw("Invalid pid");
             }
