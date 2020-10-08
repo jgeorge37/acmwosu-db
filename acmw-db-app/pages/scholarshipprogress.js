f59734e4c6d5f2fc3524247f251b9bbc6c70f16d
Sara Miskus <51302701+smiskus@users.noreply.github.com>
2020-10-08 17:15:58 -0400

    Szm/ada 56 user attendance (#33)
    * Fixing formatting
    
    * ADA-56 Adding fall/spring attendance display
    
    * ADA-56 Adding event number to progress bar calculations, fixing some formatting
    
    * ADA 56 Fixed unnecessary variable

--- acmw-db-app/pages/scholarshipprogress.js
+++ acmw-db-app/pages/scholarshipprogress.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Database.module.css'
 import progress_styles from '../styles/components/ScholarshipProgressBar.module.css'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import ScholarshipProgressBar from '../components/ScholarshipProgressBar'
 import NavBar from '../components/NavBar'
 
@@ -11,7 +11,7 @@ const ScholarshipProgress = () => {
     const requiredAmountForExternalScholarship = 1;
     const requiredAmountForVolunteerHours = 4;
     const requiredAmountForMeetings = 10;
-
+    const requiredAmountForSemester = 5;
 
     // For future, these will be passed in from Admin input
 
@@ -19,6 +19,31 @@ const ScholarshipProgress = () => {
     // work with this to be choose: category, amount (either: single-scholarship, hours, or single-meeting),
     // and string comment of details like the date and have that passed into corresponding list
     // then the progress calculated from the inputted amount matched to category
+
+    // We'll need to make sure to current user logged will be passed in
+    const currentUserEmail = 'miskus.1@osu.edu';
+    const [fallMeetings, setFallMeetings] = useState(0);
+    const [springMeetings, setSpringMeetings] = useState(0);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const requestOptions = {
+                method: 'GET',
+            };
+            console.log("here")
+            const api = '/api/meeting/account-attendance?email=' + currentUserEmail;
+            const res = await fetch(api, requestOptions);
+            const result = await res.json();           
+            for (var i = 0; i < result.length; i++) {
+                if (result[i].semester.indexOf("AU") >= 0) {
+                    setFallMeetings(result[i].count)    
+                } else {
+                    setSpringMeetings(result[i].count)
+                }
+            }
+        }
+        fetchData()
+    }, [])
 
     // pass in string of details, added to list
     const testScholarshipList = ['External Scholarship: AnitaB'];
@@ -29,6 +54,7 @@ const ScholarshipProgress = () => {
     // pass in string of details, added to list
     const testMeetingList = ['ACM-W Meeting 9/2','ACM-W Meeting 9/9','ACM-W Meeting 9/16','ACM-W Meeting 9/23','ACM-W Meeting 9/30','ACM-W Meeting 10/7'];
 
+    
     function calculateExternalScholarshipProgress(testScholarshipList, requiredAmountForExternalScholarship) {
         let percentExternScholarship = (testScholarshipList.length / requiredAmountForExternalScholarship) * 100;
         return percentExternScholarship;
@@ -44,8 +70,9 @@ const ScholarshipProgress = () => {
         return percentVolunteerHours;
     }
 
-    function calculateMeetingProgress(testMeetingList, requiredAmountForMeetings) {
-        let percentMeetings = (testMeetingList.length / requiredAmountForMeetings) * 100;
+    function calculateMeetingProgress(numMeetings, requiredAmountForMeetings) {
+        // let percentMeetings = (testMeetingList.length / requiredAmountForMeetings) * 100;
+        let percentMeetings = (numMeetings / requiredAmountForMeetings) * 100;
         return percentMeetings;
     }
 
@@ -93,8 +120,15 @@ const ScholarshipProgress = () => {
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td>ACM-W Meetings</td>
-                                    <td><ScholarshipProgressBar completed={calculateMeetingProgress(testMeetingList, requiredAmountForMeetings)}/></td>
+                                    <td>ACM-W Meetings Fall: {fallMeetings} attended</td>
+                                    <td><ScholarshipProgressBar completed={calculateMeetingProgress(fallMeetings, requiredAmountForSemester)}/></td>
+                                    <td>
+                                        <div className={progress_styles.list_scroll}>{testMeetingItems}</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>ACM-W Meetings Spring: {springMeetings} attended</td>
+                                    <td><ScholarshipProgressBar completed={calculateMeetingProgress(springMeetings, requiredAmountForSemester)}/></td>
                                     <td>
                                         <div className={progress_styles.list_scroll}>{testMeetingItems}</div>
                                     </td>
