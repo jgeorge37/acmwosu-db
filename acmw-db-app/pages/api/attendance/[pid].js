7afbb4314aa77700fb440ad7a5a603588073fb7b
cachemeifyoucan0 <masonmilly@gmail.com>
2021-01-03 17:15:58 -0500

    fixed nowers

--- acmw-db-app/pages/api/attendance/[pid].js
+++ acmw-db-app/pages/api/attendance/[pid].js
@@ -1,30 +1,32 @@
 import pgQuery from '../../../postgres/pg-query.js';
+//idk I wanna do this somehow but need to export it from student? thots?
+//import {createStudent} from '../student/[pid].js'; 
 
 // POST /api/attendance/record
-// Create an account: email = new user's email address, password = unencrypted new password, student_id may be null
+// Record a specific student attendance
+// event_code: code for meeting, f_name: first name of student, 
+// l_name_dot_num: last name and dot number of student, year_level: school level of student
 async function record (event_code, f_name, l_name_dot_num, year_level) {
-    const curr_student_id = await pgQuery(`SELECT student_id FROM student WHERE name_dot_num = '${l_name_dot_num}'`);
+    const curr_student_id = await pgQuery(`SELECT id FROM student WHERE name_dot_num = '${l_name_dot_num}'`);
+    //if the student doesn't exist 
     if(!curr_student_id.rowCount){
        let l_name = l_name_dot_num.split(/\./)[0];
-       const reqCreateStudent = {
-         method: 'POST',
-         body: JSON.stringify(
-           { fname: f_name.charAt(0).toUpperCase() + f_name.slice(1).toLowerCase(), //capitalization convention
-             lname: l_name.charAt(0).toUpperCase() + l_name.slice(1).toLowerCase(), //capitalization convention
-             name_dot_num: l_name_dot_num.toLowerCase(),
-             personal_email: "", 
-             school_level: year_level,
-             packet_sent_date: ""
-           }
-         )
-       };
-       // using exisiting end point to create student
-       const res1 = await fetch('/api/student/create', reqCreateStudent);
-       const result1 = await res1.json(); //returns the id
-       curr_student_id = result1[0]["id"];
+       // creating new student if it didn't exist
+       // so, right now should get
+       //  error: insert or update on table "meeting_student" violates foreign key constraint "meeting_student_meeting_id_fkey"
+       curr_student_id = await pgQuery(`
+            INSERT INTO student (fname, lname, name_dot_num, personal_email, school_level, packet_sent_date)
+                VALUES ('${f_name}', '${l_name}', '${l_name_dot_num}',
+                 '${no_email}',
+                 '${year_level}',
+                  '${no_send_packet}')
+         RETURNING id;
+         `);
     }
+
+    // assuming meeting id is present for now, so not adding it to meeting table, just straight to meeting student
     const data = await pgQuery(`INSERT INTO meeting_student (meeting_id, student_id)
-        VALUES ('${event_code}', ${curr_student_id}')`);
+        VALUES ('${event_code}', '${curr_student_id.rows[0].id}')`);
     console.log(data);
     return data;
 }
@@ -41,8 +43,9 @@ export default async (req, res) => {
             const body = typeof(req.body) === 'object' ? req.body : JSON.parse(req.body);
             switch(pid) {
                 case 'record':
-                    result = await record(body.event_code, body.f_name, body.l_name_dot_num);
+                    result = await record(body.event_code, body.f_name, body.l_name_dot_num, body.year_level);
                     break;
+                // probs need to add more error checks in future
                 default:
                     throw("Invalid meeting_id");
             }
