f2e5777509d8f26a8a5796d13f27a33b4736e682
Jing George <jing.c.geo@gmail.com>
2021-01-12 21:04:25 -0500

    Hotfix: change token_expire_time to reset_expire_time in account API

--- acmw-db-app/pages/api/account/[pid].js
+++ acmw-db-app/pages/api/account/[pid].js
@@ -20,7 +20,7 @@ async function generateToken (email) {
     // save token and set expiration time 2 days in the future
     const data = await pgQuery(`
         UPDATE account
-        SET reset_token = '${token}', token_expire_time = current_timestamp + INTERVAL '1 hour'
+        SET reset_token = '${token}', reset_expire_time = current_timestamp + INTERVAL '1 hour'
         WHERE email = '${email}';
     `);
 
@@ -33,7 +33,7 @@ async function checkReset (token) {
     // Get the email address associated with a token if token is not expired
     const validResetWindow = await pgQuery(`
         SELECT email FROM account
-        WHERE current_timestamp <= token_expire_time AND reset_token = '${token}';
+        WHERE current_timestamp <= reset_expire_time AND reset_token = '${token}';
     `);
 
     return validResetWindow.rows;
@@ -47,7 +47,7 @@ async function resetPassword (email, password) {
         await pgQuery(`
             UPDATE account SET
                 reset_token = null,
-                token_expire_time = null,
+                reset_expire_time = null,
                 password = crypt('${password}', gen_salt('md5'))
             WHERE email = '${email}';
         `);
